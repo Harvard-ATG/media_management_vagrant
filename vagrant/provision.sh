37a9a31ad43e52352c2e5fdc6269bd0c1dcc96b8
Arthur Barrett <abarrett@fas.harvard.edu>
2017-03-07 12:42:34 -0500

    Run API by default using UWSGI. Run LTI manually with manage.py when developing.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -8,22 +8,36 @@ sudo apt-get update
 sudo apt-get -y autoremove
 
 # Install system packages
-sudo apt-get -y install build-essential libffi-dev
-sudo apt-get -y install libxslt1-dev libxml2 libxml2-dev
-sudo apt-get -y install openssl libcurl4-openssl-dev
-sudo apt-get -y install git curl wget unzip
+sudo apt-get -y install build-essential libffi-dev zlib1g-dev libjpeg8-dev git curl wget unzip
+sudo apt-get -y install libxslt1-dev libxml2 libxml2-dev openssl libcurl4-openssl-dev
 sudo apt-get -y install python-dev python-pip python-setuptools redis-server
 sudo apt-get -y install postgresql postgresql-contrib libpq-dev
-sudo apt-get -y install zlib1g-dev libjpeg8-dev
 sudo apt-get -y install nginx
-
 sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install system python dependencies (project-specific will be installed in a virtualenv)
-sudo -H pip install virtualenv
-sudo -H pip install urllib3[secure]
-sudo -H pip install uwsgi
+sudo -H pip install virtualenv urllib3[secure] uwsgi virtualenvwrapper
+
+# Ensure github.com ssh public key in known_hosts file
+chmod 700 $HOME/.ssh
+if ! grep -sq github.com $HOME/.ssh/known_hosts; then
+	ssh-keyscan github.com >> $HOME/.ssh/known_hosts
+fi
+chmod 644 $HOME/.ssh/known_hosts
+
+# Setup bash_profile
+if ! grep -sq WORKON_HOME $HOME/.bash_profile; then
+	cat <<__END >> $HOME/.bash_profile
+source .bashrc
+export WORKON_HOME=/opt/virtualenvs
+export PROJECT_HOME=/srv
+source /usr/local/bin/virtualenvwrapper.sh
+__END
+fi
+
+# Ensure permissions are correct in home directory
+chown -R $USER:$USER $HOME
 
 # Setup database
 for project in ${PROJECTS[@]}
@@ -34,19 +48,7 @@ do
 	sudo -u postgres -i psql -d postgres -c "CREATE DATABASE $project WITH OWNER $project"
 done
 
-# Ensure permissions are correct
-sudo chown -R www-data:www-data /srv
-
-sudo -u $USER -s bash
-
-# Ensure github.com ssh public key is in $HOME/.ssh/known_hosts file
-chmod 700 $HOME/.ssh
-if ! grep -sq github.com $HOME/.ssh/known_hosts; then
-	ssh-keyscan github.com >> $HOME/.ssh/known_hosts
-fi
-chmod 744 $HOME/.ssh/known_hosts
-
-# Install project requirements
+# Setup requirements
 for project in ${PROJECTS[@]}
 do
 	mkdir -pv /opt/virtualenvs/$project
@@ -56,9 +58,9 @@ do
 done
 
 # uwsgi
-sudo cp /opt/provision/upstart/uwsgi.conf /etc/init/uwsgi.conf
+sudo cp -fv /opt/provision/upstart/uwsgi.conf /etc/init/uwsgi.conf
 sudo service uwsgi stop
 
 # nginx
-sudo ln -sf /opt/provision/nginx/apps.conf /etc/nginx/sites-enabled/apps
+sudo ln -sfv /opt/provision/nginx/media_management_api.conf /etc/nginx/sites-enabled/media_management_api
 sudo service nginx stop
